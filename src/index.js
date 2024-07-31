0b35395839e70d6a14c462cedf73cdbe38a38c2d
Nikita Malinovsky <nikitamalinovsky3@gmail.com>
2024-07-31 10:24:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    let result = '';
+
+    let string = n.toString();
   
+    for(let i = string.length - 1; i >= 0; i--){
+        if(string[i] !== "-"){
+            result += string[i];
+        }
+    }
+  
+    return Number(result);
 }
